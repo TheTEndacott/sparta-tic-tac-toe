201edf144807cecb198548deee120973e85bcd3b
Tom Endacott <tendacott@spartaglobal.com>
2018-07-04 18:51:12 +0100

    Added updated win message

--- script.js
+++ script.js
@@ -26,21 +26,21 @@ $(function() {
 
   function checkWin(){
     if (board[0] == "X" && board[1] == "X" && board[2] == "X" || board[0] == "O" && board[1] == "O" && board[2] == "O") {
-      $("table").replaceWith("<h1>WINNER WINNER CHICKEN DINNER</h1>");
+      $("table").html("<h1>WINNER WINNER CHICKEN DINNER</h1>").css("color", "green");
     }else if (board[3] == "X" && board[4] == "X" && board[5] == "X" || board[3] == "O" && board[4] == "O" && board[5] == "O" ) {
-      $("table").replaceWith("<h1>WINNER WINNER CHICKEN DINNER</h1>");
+      $("table").html("<h1>WINNER WINNER CHICKEN DINNER</h1>").css("color", "green");
     }else if (board[6] == "X" && board[7] == "X" && board[8] == "X" || board[6] == "O" && board[7] == "O" && board[8] == "O" ){
-      $("table").replaceWith("<h1>WINNER WINNER CHICKEN DINNER</h1>");
+      $("table").html("<h1>WINNER WINNER CHICKEN DINNER</h1>").css("color", "green");
     }else if (board[2] == "X" && board[5] == "X" && board[8] == "X" || board[2] == "O" && board[5] == "O" && board[8] == "O" ){
-      $("table").replaceWith("<h1>WINNER WINNER CHICKEN DINNER</h1>");
+      $("table").html("<h1>WINNER WINNER CHICKEN DINNER</h1>").css("color", "green");
     }else if (board[1] == "X" && board[4] == "X" && board[7] == "X" || board[1] == "O" && board[4] == "O" && board[7] == "O" ){
-      $("table").replaceWith("<h1>WINNER WINNER CHICKEN DINNER</h1>");
+      $("table").html("<h1>WINNER WINNER CHICKEN DINNER</h1>").css("color", "green");
     }else if (board[0] == "X" && board[3] == "X" && board[6] == "X" || board[0] == "O" && board[3] == "O" && board[6] == "O" ){
-      $("table").replaceWith("<h1>WINNER WINNER CHICKEN DINNER</h1>");
+      $("table").html("<h1>WINNER WINNER CHICKEN DINNER</h1>").css("color", "green");
     }else if (board[0] == "X" && board[4] == "X" && board[8] == "X" || board[0] == "O" && board[4] == "O" && board[8] == "O" ){
-      $("table").replaceWith("<h1>WINNER WINNER CHICKEN DINNER</h1>");
+      $("table").html("<h1>WINNER WINNER CHICKEN DINNER</h1>").css("color", "green");
     }else if (board[2] == "X" && board[4] == "X" && board[6] == "X" || board[2] == "O" && board[4] == "O" && board[6] == "O" ){
-      $("table").replaceWith("<h1>WINNER WINNER CHICKEN DINNER</h1>");
+      $("table").html("<h1>WINNER WINNER CHICKEN DINNER</h1>").css("color", "green");
     }
   }
 
